53f533cb76fa74182480b86de193097d41718605
Blaise Hala <blaisehala@gmail.com>
2022-02-06 19:01:51 +0300

    init:resource addition:Modification of files

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,4 @@
 
-//defining the object for the question entity
-
-let question = {
-  title: 'which HTML element do we put the JavaScript',
-  choices: [
-    'javascript',
-    
-    'Scripting',
-    
-    'script',
-    
-    'js'
-    
-    ],
-  correctAnswer: 2
-};
 
 
-//To keep our code organized and flexible we will create a JavaScript Function to show the question ( showQuestion ). This will enable us to execute the function by calling it, passing in the question object.
+
